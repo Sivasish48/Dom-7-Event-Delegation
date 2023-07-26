437a200e7e75942f7823bbf082c6c8c8700c4d7c
Sivasish Praharaj <98220976+Sivasish48@users.noreply.github.com>
2023-07-26 21:47:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// 1. INTRODUCTION TO EVENT DELEGATION
+
+// Event Delegation is an event handling pattern that allows users to append a Single event listener to a parent element that adds it to all of its present AND future descendants that match a selector.
+// Event Deegation is a pattern based upon the concept of Event bubbling.
+// By default , events triggered on an element propagate up the DOM tre to the element's parent , its ancestors and on up until the root element(html).
