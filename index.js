40671d211861612732626afbcc4e85c466eeb8de
Sivasish Praharaj <98220976+Sivasish48@users.noreply.github.com>
2023-07-27 10:05:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,27 @@
 // Event Delegation is an event handling pattern that allows users to append a Single event listener to a parent element that adds it to all of its present AND future descendants that match a selector.
 // Event Deegation is a pattern based upon the concept of Event bubbling.
 // By default , events triggered on an element propagate up the DOM tre to the element's parent , its ancestors and on up until the root element(html).
+document.querySelector("#foods").addEventListener("click", function(e){
+    const getid = e.target.getAttribute("id");
+    console.log(` ${getid} is clicked `);
+
+    const target = e.target;
+
+    if (target.matches('button')) {
+        target.textContent = ` ${getid} is clicked `;
+    }
+    
+});
+
+// 1.3 ADDING FUTURE DESCENDANTS
+// So here below is an example of a future descendant to be in the process.
+
+  const foods = document.querySelector('#foods');
+  
+  const newFood = document.createElement("button");
+  
+  newFood.innerText = "Hotdog";
+  newFood.setAttribute('id','hotdog');
+
+  foods.appendChild(newFood);
+
